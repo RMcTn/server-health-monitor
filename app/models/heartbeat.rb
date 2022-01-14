b7dd9ddc78e5345d78dd0c2795530db701b8ca28
Reece McTiernan <rmctiernan@pm.me>
2022-01-14 13:34:44 +0000

    Add live updating of server indicator

--- app/models/heartbeat.rb
+++ app/models/heartbeat.rb
@@ -3,5 +3,8 @@ class Heartbeat < ApplicationRecord
 
   after_create_commit {
     broadcast_prepend_to server
+    
+    server_header_dom_id = "server_" + server.id.to_s + "-server-header"
+    broadcast_replace_to(server, target: str, partial: 'servers/server_header', locals: {server: server})
   }
 end
