43bf0a2bd02fc7fa2b912f102209174b9f0cc6fa
Reece McTiernan <rmctiernan@pm.me>
2022-01-18 16:18:27 +0000

    Delete servers, org_users when org is deleted

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,6 +1,6 @@
 class Organisation < ApplicationRecord
   validates :name, presence: true
-  has_many :servers
-  has_many :organisations_users
+  has_many :servers, dependent: :destroy
+  has_many :organisations_users, dependent: :destroy
   has_many :users, through: :organisations_users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :organisations_users
+  has_many :organisations_users, dependent: :destroy
   has_many :organisations, through: :organisations_users
 
 end
