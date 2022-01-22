f4695f06c45926c61d6f94384f00a119f8d3ff72
Reece McTiernan <rmctiernan@pm.me>
2022-01-22 22:29:08 +0000

    Fix word wrap on heartbeat, and catch specific SocketError for job

--- app/jobs/send_heartbeat_job.rb
+++ app/jobs/send_heartbeat_job.rb
@@ -7,7 +7,6 @@ class SendHeartbeatJob < ApplicationJob
   end
 
   def perform(server)
-    # TODO: need a queue backend for this for persistence
     uri = URI.parse(server.protocol + server.hostname)
     request_time = Time.now
     # TODO: Will need a timeout to stop getting hung
@@ -15,8 +14,7 @@ class SendHeartbeatJob < ApplicationJob
       res = Net::HTTP.get_response(uri)
       response_time = Time.now
       heartbeat = server.heartbeats.create(status_code: res.code, status_message: res.message, request_time: request_time, response_time: response_time)
-    rescue Exception => e
-      # TODO: Check if different HTTP lib will handle things like google.com instead of http://www.google.com
+    rescue Errno::ECONNREFUSED, SocketError  => e
       heartbeat = server.heartbeats.create(status_code: 0, request_time: request_time, status_message: e.message)
     end
   end
